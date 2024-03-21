605abd755ecb065538747fa0c429c914a81a0782
Glitchyi <advaithnarayanan8@gmail.com>
2024-03-21 23:11:00 +0530

    loops

--- 3-loops/src/main.rs
+++ 3-loops/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    // For loop example
+    println!("For loop example:");
+    for i in 1..5 {
+        println!("i = {}", i);
+    }
+
+    // While loop example
+    println!("\nWhile loop example:");
+    let mut j = 5;
+    while j > 0 {
+        println!("j = {}", j);
+        j -= 1;
+    }
+
+    // Loop (infinite loop) example with break
+    println!("\nLoop (infinite loop) example with break:");
+    let mut count = 0;
+    loop {
+        println!("Looping...");
+        count += 1;
+        if count == 3 {
+            println!("Reached count = 3, breaking out of the loop.");
+            break;
+        }
+    }
 }
+
